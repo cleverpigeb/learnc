04c7c11ef7476f2dd25420d8bd2b633099706e6a
xuesong3 <xuesong3@staff.sina.com.cn>
2017-04-08 21:56:38 +0800

    init

--- 2/test/2_7/2_7.c
+++ 2/test/2_7/2_7.c
@@ -1,3 +1,22 @@
 /*习题 2-7*/
 
 #include <stdio.h>
+
+int main(void)  {
+	double a;
+	int i;
+
+	a = 0.0;
+
+	for(i = 1; 2 * i - 1 < 1000000; i++) {
+		if(i % 2 == 1) {
+			a += 1.0 / (2 * i - 1);
+		} else {
+			a -= 1.0 / (2 * i - 1);
+		}		
+	}
+
+	printf("%lf\n", a);
+
+	return 0;
+}
